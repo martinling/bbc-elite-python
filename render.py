cf5b99cc13def5fbd6043b89c21093bb0e7a3be4
Martin Ling <martin-git@earth.li>
2018-08-23 23:12:18 +0100

    Decode rotation.

--- render.py
+++ render.py
@@ -1,9 +1,16 @@
 import sys
 import numpy as np
 
-def int24(b):
-	magnitude = (b[2] & 0x7F) << 16 | b[1] << 8 | b[0]
-	sign = -1 if b[2] & 0x80 else 1
+def int24(x):
+	b = x.reshape(-1, 3).astype(np.uint32)
+	magnitude = (b[:,2] & 0x7F) << 16 | b[:,1] << 8 | b[:,0]
+	sign = np.where(b[:,2] & 0x80, -1, 1)
+	return sign * magnitude
+
+def int16(x):
+	b = x.reshape(-1, 2).astype(np.uint32)
+	magnitude = (b[:,1] & 0x7F) << 8 | b[:,0]
+	sign = np.where(b[:,1] & 0x80, -1, 1)
 	return sign * magnitude
 
 while True:
@@ -12,8 +19,8 @@ while True:
 	for i, ship in enumerate(data):
 		if np.all(ship == 0):
 			continue
-		pos = map(int24, ship[0:9].reshape(3, 3))
-		rot = ship[9:27].view(dtype=np.int16).reshape(3,3)
+		pos = int24(ship[0:9])
+		rot = int16(ship[9:27]).reshape(3,3) / float(0x6000)
 		rest = ship[27:36]
 		print "Ship ", i
 		print "Position:", pos
