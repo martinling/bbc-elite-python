6b25671b3cfc581419f56ac3b23c4481c16b6d0d
Martin Ling <martin-git@earth.li>
2018-08-23 22:53:06 +0100

    Decode position.

--- render.py
+++ render.py
@@ -1,13 +1,18 @@
 import sys
 import numpy as np
 
+def int24(b):
+	magnitude = (b[2] & 0x7F) << 16 | b[1] << 8 | b[0]
+	sign = -1 if b[2] & 0x80 else 1
+	return sign * magnitude
+
 while True:
 	buf = sys.stdin.read(13*36)
 	data = np.frombuffer(buf, dtype=np.uint8).reshape(13,36)
 	for i, ship in enumerate(data):
 		if np.all(ship == 0):
 			continue
-		pos = ship[0:9]
+		pos = map(int24, ship[0:9].reshape(3, 3))
 		rot = ship[9:27].view(dtype=np.int16).reshape(3,3)
 		rest = ship[27:36]
 		print "Ship ", i
