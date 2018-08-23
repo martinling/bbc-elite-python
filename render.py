2c041f88b6727b1e981a1561483f24c1213191ac
Martin Ling <martin-git@earth.li>
2018-08-23 22:19:58 +0100

    Begin breaking down ship state data.

--- render.py
+++ render.py
@@ -3,5 +3,15 @@ import numpy as np
 
 while True:
 	buf = sys.stdin.read(13*36)
-	data = np.frombuffer(buf, dtype=np.uint8).reshape((13,36))
-	print data
+	data = np.frombuffer(buf, dtype=np.uint8).reshape(13,36)
+	for i, ship in enumerate(data):
+		if np.all(ship == 0):
+			continue
+		pos = ship[0:9]
+		rot = ship[9:27].view(dtype=np.int16).reshape(3,3)
+		rest = ship[27:36]
+		print "Ship ", i
+		print "Position:", pos
+		print "Rotation:"
+		print rot
+		print "State:", rest
