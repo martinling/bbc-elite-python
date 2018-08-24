b23a331e452d6507b5a092b117ff2d5f5d7a427d
Martin Ling <martin-git@earth.li>
2018-08-24 01:34:45 +0100

    Render ships as cubes in VTK.

--- render.py
+++ render.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+from vtk import *
 
 def int24(x):
 	b = x.reshape(-1, 3).astype(np.uint32)
@@ -13,10 +14,22 @@ def int16(x):
 	sign = np.where(b[:,1] & 0x80, -1, 1)
 	return sign * magnitude
 
+source = vtkCubeSource()
+camera = vtkCamera()
+camera.SetPosition(0, 0, 0)
+camera.SetFocalPoint(0, 0, 100)
+renderer = vtkRenderer()
+renderer.SetActiveCamera(camera)
+window = vtkRenderWindow()
+window.AddRenderer(renderer)
+
+transforms = [None] * 13
+filters = [None] * 13
+
 while True:
 	buf = sys.stdin.read(13*36)
 	data = np.frombuffer(buf, dtype=np.uint8).reshape(13,36)
-	for i, ship in enumerate(data):
+	for i, ship in enumerate(data[1:]):
 		if np.all(ship == 0):
 			continue
 		pos = int24(ship[0:9])
@@ -27,3 +40,22 @@ while True:
 		print "Rotation:"
 		print rot
 		print "State:", rest
+		if not transforms[i]:
+			transforms[i] = vtkTransform()
+			transforms[i].PostMultiply()
+			filters[i] = vtkTransformPolyDataFilter()
+			filters[i].SetInputConnection(source.GetOutputPort())
+			filters[i].SetTransform(transforms[i])
+			mapper = vtkPolyDataMapper()
+			mapper.SetInputConnection(filters[i].GetOutputPort())
+			actor = vtkActor()
+			actor.SetMapper(mapper)
+			renderer.AddActor(actor)
+		matrix = np.empty((4,4))
+		matrix[0:3,0:3] = (rot * np.array([-1, 1, 1])).T
+		matrix[0:3,3] = pos * np.array([-1, 1, 1]) / float(200)
+		matrix[3,0:3] = 0
+		matrix[3,3] = 1
+		transforms[i].SetMatrix(matrix.reshape(16))
+		filters[i].Update()
+	window.Render()
