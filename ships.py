ee105a00f66b953818cf7deb83dafdec6356242c
Martin Ling <martin-git@earth.li>
2018-08-27 02:10:13 +0100

    Use game class in ships.py.

--- ships.py
+++ ships.py
@@ -3,14 +3,14 @@ from vtk import *
 from structures import *
 import sys
 
+game = Game()
+
 ram = np.frombuffer(sys.stdin.read(0x10000), dtype=np.uint8)
-offsets = ram[0x5600:0x5600+64].view(np.uint16)
-for offset in offsets:
-	if offset == 0:
-		continue
-	try:
-		ship = ShipData(ram, offset)
-	except ValueError:
+
+game.update(ram)
+
+for ship in game.ship_data:
+	if not ship:
 		continue
 	mapper = vtkPolyDataMapper()
 	mapper.SetInputData(ship.poly)
