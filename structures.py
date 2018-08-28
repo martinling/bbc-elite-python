577edbd170b3d4e98117613853c2dbb650ba7088
Martin Ling <martin-git@earth.li>
2018-08-28 10:49:07 +0100

    Reload station model when a new staton spawns.

--- structures.py
+++ structures.py
@@ -93,24 +93,29 @@ class Game(object):
 		# Get addresses where ship data is loaded
 		self.ship_addrs = ram[0x5600:0x5600+64].view(np.uint16)
 
+		# Read ship types. Check if a new space station has spawned.
+		ship_types = ram[0x311:0x311 + 13]
+		new_station = (2 in ship_types) and not (2 in self.ship_types)
+		self.ship_types = ship_types
+
 		# Read data for any ships we don't already know
 		for i, addr in enumerate(self.ship_addrs):
 			if addr == 0:
 				# Ship type not currently loaded
 				continue
 			elif self.ship_data[i]:
-				# Already read this ship type
-				continue
-			else:
-				# Read this ship type
-				try:
-					self.ship_data[i] = ShipData(ram, addr)
-				except ValueError:
+				if i == 1 and new_station:
+					# Space station, two versions of model.
 					pass
+				else:
+					# Already read this ship type
+					continue
+			# Read this ship type
+			try:
+				self.ship_data[i] = ShipData(ram, addr)
+			except ValueError:
+				pass
 
 		# Read ship states
 		self.ship_states = [ShipState(state)
 			for state in ram[0x900:0x900 + 13*37].reshape(13,37)]
-
-		# Read ship types
-		self.ship_types = ram[0x311:0x311 + 13]
