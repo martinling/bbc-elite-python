d4b36e51bbbb9310e5da199905ca82ab5beaab11
Martin Ling <martin-git@earth.li>
2018-08-30 16:00:39 +0100

    First try at rendering something.

--- laser.py
+++ laser.py
@@ -7,6 +7,9 @@ ol.init()
 
 game = Game()
 
+ol.perspective(60, 1, 1, 100)
+ol.translate3((0, 0, -100))
+
 while True:
 	# Read RAM from emulator
 	ram = np.frombuffer(sys.stdin.buffer.read(0x10000), dtype=np.uint8)
@@ -14,13 +17,27 @@ while True:
 	# Update game state
 	game.update(ram)
 
+	ol.renderFrame(60)
+
 	for ship_type, state, ship in zip(
 			game.ship_types,
 			game.ship_states,
 			game.ship_data):
+
 		if ship_type == 0:
 			continue
+
 		print("Type:", ship_type)
 		print("Position:", state.pos)
 		print("Rotation:")
 		print(state.rot)
+
+		if not ship:
+			continue
+
+		for edge in ship.edges[:4]:
+			ol.begin(ol.LINESTRIP)
+			for vertex in ship.vertices[edge]:
+				print(vertex)
+				ol.vertex3(vertex, ol.C_WHITE)
+			ol.end()
