7bbb429c522235e879f31638f526a7d8436682f7
Martin Ling <martin-git@earth.li>
2018-08-30 15:21:33 +0100

    Fix structures.py for python 3.

--- structures.py
+++ structures.py
@@ -43,10 +43,10 @@ class ShipData(object):
 		self.face_offset = header[[4,17]].view(np.uint16)[0]
 		self.heap_size = header[5]
 		self.laser_vertex = header[6]
-		self.num_vertices = header[8] / 6
+		self.num_vertices = header[8] // 6
 		self.num_edges = header[9]
 		self.bounty = header[10:12].view(np.uint16)[0]
-		self.num_faces = header[12] / 4
+		self.num_faces = header[12] // 4
 		self.dot_distance = header[13]
 		self.max_energy = header[14]
 		self.max_speed = header[15]
@@ -61,7 +61,7 @@ class ShipData(object):
 		face_data = data[:4*self.num_faces].reshape(-1,4)
 		self.vertices = vertices(vertex_data)
 		self.vertex_faces = nibbles(vertex_data[:,4:6])
-		self.edges = edge_data[:,2:4] / 4
+		self.edges = edge_data[:,2:4] // 4
 		self.edge_faces = nibbles(edge_data[:,1:2])
 		self.normals = normals(face_data)
 		if np.any(self.edges >= self.num_vertices):
