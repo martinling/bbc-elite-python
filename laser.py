863e23069d00aa2cbfd6582b0d999dd990a0a143
Martin Ling <martin-git@earth.li>
2018-08-30 17:51:47 +0100

    Translate wireframes to position.

--- laser.py
+++ laser.py
@@ -37,6 +37,10 @@ while True:
 		if not ship:
 			continue
 
+		ol.pushMatrix3()
+
+		ol.translate3(tuple(state.pos * [1, 1, -1]))
+
 		graph = networkx.Graph()
 		graph.add_nodes_from(range(ship.num_vertices))
 		graph.add_edges_from(ship.edges)
@@ -49,5 +53,7 @@ while True:
 			for vertex in vertices:
 				ol.vertex3(ship.vertices[vertex], ol.C_WHITE)
 			ol.end()
+
+		ol.popMatrix3()
 
 	ol.renderFrame(60)
