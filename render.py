cd8d32b7dbde9069481f971009ed2898647dbb03
Martin Ling <martin-git@earth.li>
2018-09-01 13:11:06 +0100

    First pass at dust rendering in VTK.

--- render.py
+++ render.py
@@ -21,6 +21,29 @@ for instance in instances:
 
 game = Game()
 
+def make_dust(game):
+	points = vtkPoints()
+	points.SetNumberOfPoints(game.num_dust * 2)
+	lines = vtkCellArray()
+	positions = (game.dust_positions) / 200.0
+	speed = [0, 0, game.speed / 200.0]
+	for i, speck in enumerate(positions):
+		points.SetPoint(2*i, speck)
+		points.SetPoint(2*i+1, speck - speed)
+		line = vtkLine()
+		line.GetPointIds().SetId(0, 2*i)
+		line.GetPointIds().SetId(1, 2*i+1)
+		lines.InsertNextCell(line)
+	poly = vtkPolyData()
+	poly.SetPoints(points)
+	poly.SetLines(lines)
+	return poly
+
+dust_mapper = vtkPolyDataMapper()
+dust_actor = vtkActor()
+dust_actor.SetMapper(dust_mapper)
+renderer.AddActor(dust_actor)
+
 while True:
 	# Read RAM from emulator
 	ram = np.frombuffer(sys.stdin.read(0x10000), dtype=np.uint8)
@@ -38,5 +61,7 @@ while True:
 			print "Speed", state.speed, "Accel", state.accel, "Roll", state.roll, "Pitch", state.pitch
 			print "Energy", state.energy, "Attack", hex(state.attack), "Behaviour", hex(state.behaviour), "Visiblity", hex(state.visibility)
 		instance.update(game)
+
+	dust_mapper.SetInputData(make_dust(game))
 
 	window.Render()
