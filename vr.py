4b48eded85b45a51e8636ae866bbc8b325bdc3a3
Martin Ling <martin-git@earth.li>
2024-05-27 00:02:35 +0100

    Add initial VR renderer.

---
+++ vr.py
@@ -0,0 +1,38 @@
+import numpy as np
+from vtk import *
+from structures import *
+from rendering import *
+from server import *
+
+server = Server()
+client = server.accept()
+
+game = Game()
+ram = client.update_blocking()
+game.update(ram)
+
+renderer = vtkOpenVRRenderer()
+renderWindow = vtkOpenVRRenderWindow()
+renderWindow.Initialize()
+
+renderWindow.AddRenderer(renderer)
+renderWindowInteractor = vtkOpenVRRenderWindowInteractor()
+renderWindowInteractor.SetRenderWindow(renderWindow)
+
+instances = [ShipInstance(i) for i in range(13)]
+for instance in instances:
+    renderer.AddActor(instance.actor)
+renderer.SetBackground(0.0, 0.0, 0.0)
+
+renderWindow.Render()
+renderWindowInteractor.Initialize()
+
+while True:
+    renderWindowInteractor.DoOneEvent(renderWindow, renderer)
+
+    if (ram := client.update_nonblocking()) is not None:
+        game.update(ram)
+        for instance in instances:
+            if game.ship_types[instance.slot] != 0:
+                state = game.ship_states[instance.slot]
+            instance.update(game)
